21ba1ea36fbed3d66987267a0ace90625e26ca67
Luděk Kánský <kansky@softpc.cz>
2022-09-19 12:33:18 +0200

    Oprava porovnání výsledků ..

--- Models/Vysledek.cs
+++ Models/Vysledek.cs
@@ -17,20 +17,15 @@ public class Vysledek : IComparable
     [ForeignKey(nameof(DruzstvoID))]
     public Druzstvo Druzstvo { get; set; }
 
-    private DateTime? Dejmi(Vysledek vysledek)
+    private static DateTime? VratNejlepsiCas(Vysledek vysledek)
     {
-        /*
-Druzstvo:Páté družstvo 04.09.2022 0:00:17False-False vysledek=04.09.2022 0:00:17
-Druzstvo:Druhé družstvo 04.09.2022 0:00:12False-True vysledek=04.09.2022 0:00:12
-Druzstvo:Třetí družstvo 18.09.2022 0:00:12False-False vysledek=18.09.2022 0:00:12
-Druzstvo:Páté družstvo 04.09.2022 0:00:17False-False vysledek=04.09.2022 0:00:17
-Druzstvo:Čtvrté družstvo 18.09.2022 0:00:15False-False vysledek=18.09.2022 0:00:15
-Druzstvo:Třetí družstvo 18.09.2022 0:00:12False-False vysledek=18.09.2022 0:00:12
-Druzstvo:Šesté družstvo 18.09.2022 0:00:18False-False vysledek=18.09.2022 0:00:18
-Druzstvo:Čtvrté družstvo 18.09.2022 0:00:15False-False vysledek=18.09.2022 0:00:15
-        */
+        if (!vysledek.NeplatnyPokus1 && !vysledek.NeplatnyPokus2 && vysledek.Cas1 == null && vysledek.Cas2 == null) return null;
+        if (vysledek.Cas1 == null && vysledek.Cas2 == null) return null;
         if (vysledek.NeplatnyPokus1 && vysledek.NeplatnyPokus2) return null;
+
         if (vysledek.NeplatnyPokus1 && !vysledek.NeplatnyPokus2 && vysledek.Cas2 != null) return vysledek.Cas2;
+        if (!vysledek.NeplatnyPokus1 && !vysledek.NeplatnyPokus2 && vysledek.Cas1 == null && vysledek.Cas2.HasValue) return vysledek.Cas2;
+
         if (!vysledek.NeplatnyPokus1 && vysledek.NeplatnyPokus2 && vysledek.Cas1 != null) return vysledek.Cas1;
         if (!vysledek.NeplatnyPokus1 && !vysledek.NeplatnyPokus2 && vysledek.Cas1 != null && vysledek.Cas2 != null) return vysledek.Cas1;
         if (!vysledek.NeplatnyPokus1 && !vysledek.NeplatnyPokus2 && vysledek.Cas1 != null && vysledek.Cas2 == null) return vysledek.Cas1;
@@ -41,10 +36,12 @@ Druzstvo:Čtvrté družstvo 18.09.2022 0:00:15False-False vysledek=18.09.2022 0:
     public int CompareTo(object obj)
     {
         var vysledek = obj as Vysledek;
-        DateTime? vysledekA = Dejmi(this);
-        Console.WriteLine($"Druzstvo:{this.Druzstvo.Nazev} {this.Cas1}{this.NeplatnyPokus1}-{this.Cas2}{this.NeplatnyPokus2} vysledek={vysledekA}");
-        DateTime? vysledekB = Dejmi(vysledek);
+        DateTime? vysledekA = VratNejlepsiCas(this);
+        Console.WriteLine("Porovnáváme:--------------------------------");
+        Console.WriteLine($"Družstvo:{Druzstvo.Nazev} {Cas1}{NeplatnyPokus1}-{Cas2}{NeplatnyPokus2} vysledek={vysledekA}");
+        DateTime? vysledekB = VratNejlepsiCas(vysledek);
         Console.WriteLine($"Druzstvo:{vysledek.Druzstvo.Nazev} {vysledek.Cas1}{vysledek.NeplatnyPokus1}-{vysledek.Cas2}{vysledek.NeplatnyPokus2} vysledek={vysledekB}");
+        Console.WriteLine("--------------------------------------------");
 
         if (vysledekA == null && vysledekB != null) return 1;
         if (vysledekA == null && vysledekB == null) return 0;
